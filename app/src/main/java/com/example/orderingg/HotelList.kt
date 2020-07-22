6e2b658d5fe4f09e60c09eee0b3fcbc6a95ab8cd
ABISHEK NP <npabishek@gmail.com>
2020-07-22 22:54:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/orderingg/HotelList.kt
@@ -0,0 +1,11 @@
+package com.example.orderingg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HotelList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hotel_list)
+    }
+}
---
+++ app/src/main/java/com/example/orderingg/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.orderingg
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnSignIn.setOnClickListener{
+            val signin_intent = Intent(this, HotelList::class.java)
+            startActivity(signin_intent)
+
+
+         btnSignUp.setOnClickListener{
+             val signup_intent = Intent(this, HotelList::class.java)
+             startActivity(signup_intent)
+         }
+        }
+    }
+}
