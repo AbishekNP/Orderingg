ec924ab11a87f981a7cfb772456c17a871ad9736
ABISHEK NP <npabishek@gmail.com>
2020-07-23 11:30:43 +0530

    Added CardView containing hotel list

--- app/src/main/java/com/example/orderingg/HotelList.kt
+++ app/src/main/java/com/example/orderingg/HotelList.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 class HotelList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_hotel_list)
+        setContentView(R.layout.hotel_list)
+
+
     }
 }
