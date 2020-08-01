cfb1ec15726824f418deb9eb9b8a06fe5001a161
ABISHEK NP <npabishek@gmail.com>
2020-08-01 10:28:54 +0530

    Added CardView containing hotel list

--- app/src/main/java/com/example/orderingg/SecondActivity.kt
+++ app/src/main/java/com/example/orderingg/SecondActivity.kt
@@ -1,11 +1,18 @@
 package com.example.orderingg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        btnhotel.setOnClickListener {
+            val hotel_intent = Intent(this, HotelList::class.java)
+            startActivity(hotel_intent)
+        }
     }
 }
--- app/src/main/java/com/example/orderingg/SecondScreen.kt
+++ app/src/main/java/com/example/orderingg/SecondScreen.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
